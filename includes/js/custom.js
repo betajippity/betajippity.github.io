cbe288559a29cc95839c2b7754602b6a12dd69c4
Yining Karl Li <betajippity@gmail.com>
2026-01-07 14:36:16 -0800

    Initial implementation for main page sidebar TOC

--- includes/js/custom.js
+++ includes/js/custom.js
@@ -201,3 +201,214 @@ $(document).ready(function() {
     // Initial update
     updateActiveSection();
 });
+
+// Homepage Sidebar TOC - Shows post titles and expands TOCs for current post
+$(document).ready(function() {
+
+    // Only run on homepage
+    if (!$('body').hasClass('home-template')) {
+        return; // Not homepage
+    }
+
+    var sidebarToc = $('#sidebar-toc');
+    var posts = [];
+
+    // Collect all posts on the page
+    $('.post').each(function(index) {
+        var postElement = $(this);
+        var postTitle = postElement.find('.post-title a').text().trim();
+        var postUrl = postElement.find('.post-title a').attr('href');
+        var manualToc = postElement.find('.tableofcontents');
+
+        var postData = {
+            index: index,
+            element: postElement,
+            title: postTitle,
+            url: postUrl,
+            hasToc: manualToc.length > 0,
+            tocLinks: [],
+            sidebarItem: null,
+            tocList: null,
+            sections: []
+        };
+
+        // If post has a TOC, extract the links
+        if (postData.hasToc) {
+            manualToc.find('a[href*="#"]').each(function() {
+                var link = $(this);
+                var href = link.attr('href');
+                var text = link.text().trim();
+                postData.tocLinks.push({ href: href, text: text });
+            });
+        }
+
+        posts.push(postData);
+    });
+
+    // Build sidebar structure
+    if (posts.length === 0) {
+        return; // No posts found
+    }
+
+    // Add class to body to enable sidebar styles
+    $('body').addClass('has-sidebar-toc');
+
+    // Add heading
+    var tocHeading = $('<h3>Posts on This Page</h3>');
+    sidebarToc.append(tocHeading);
+
+    var mainList = $('<ul class="homepage-toc-list"></ul>');
+
+    // Create sidebar entries for each post
+    posts.forEach(function(post) {
+        var listItem = $('<li class="homepage-toc-post"></li>');
+
+        // Post title (clickable)
+        var titleLink = $('<a href="#" class="post-title-link"></a>')
+            .text(post.title)
+            .data('post-index', post.index)
+            .click(function(e) {
+                e.preventDefault();
+                var scrollTarget = post.element.offset().top - 120;
+                $('html, body').animate({ scrollTop: scrollTarget }, 300);
+            });
+
+        listItem.append(titleLink);
+
+        // If post has TOC, create collapsed TOC list (hidden initially)
+        if (post.hasToc && post.tocLinks.length > 0) {
+            var tocList = $('<ul class="post-toc-list" style="display:none;"></ul>');
+
+            post.tocLinks.forEach(function(tocLink, tocIndex) {
+                var tocItem = $('<li></li>');
+                var numberedText = (tocIndex + 1) + '. ' + tocLink.text;
+                var sectionLink = $('<a></a>')
+                    .attr('href', tocLink.href)
+                    .text(numberedText);
+
+                tocItem.append(sectionLink);
+                tocList.append(tocItem);
+            });
+
+            listItem.append(tocList);
+            post.tocList = tocList;
+
+            // Build sections array for scrollspy within this post
+            post.tocLinks.forEach(function(tocLink, tocIndex) {
+                var href = tocLink.href;
+                var hashIndex = href.indexOf('#');
+                if (hashIndex !== -1) {
+                    var targetId = href.substring(hashIndex + 1);
+                    var sectionElement = document.getElementById(targetId);
+                    if (sectionElement) {
+                        post.sections.push({
+                            element: $(sectionElement),
+                            link: tocList.find('a').eq(tocIndex)
+                        });
+                    }
+                }
+            });
+        }
+
+        post.sidebarItem = listItem;
+        mainList.append(listItem);
+    });
+
+    sidebarToc.append(mainList);
+
+    // Scrollspy: Track current post and current section
+    var currentPost = null;
+    var currentSection = null;
+
+    function updateHomepageScrollspy() {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+
+        // Find which post is currently in view (middle of viewport)
+        var viewportMiddle = scrollPos + (windowHeight / 2);
+
+        var newCurrentPost = null;
+        for (var i = posts.length - 1; i >= 0; i--) {
+            var post = posts[i];
+            var postTop = post.element.offset().top;
+            var postBottom = postTop + post.element.outerHeight();
+
+            if (viewportMiddle >= postTop && viewportMiddle <= postBottom) {
+                newCurrentPost = post;
+                break;
+            }
+        }
+
+        // If current post changed, update sidebar
+        if (newCurrentPost !== currentPost) {
+            // Remove highlight and collapse previous post's TOC
+            if (currentPost) {
+                currentPost.sidebarItem.find('.post-title-link').removeClass('active-post');
+                if (currentPost.tocList) {
+                    // Clear any active section highlights before collapsing
+                    currentPost.tocList.find('a').removeClass('active');
+                    currentPost.tocList.slideUp(200);
+                }
+            }
+
+            // Clear current section reference
+            if (currentSection) {
+                currentSection.link.removeClass('active');
+                currentSection = null;
+            }
+
+            // Highlight and expand new post's TOC
+            if (newCurrentPost) {
+                newCurrentPost.sidebarItem.find('.post-title-link').addClass('active-post');
+                if (newCurrentPost.tocList) {
+                    newCurrentPost.tocList.slideDown(200);
+                }
+            }
+
+            currentPost = newCurrentPost;
+        }
+
+        // If current post has TOC, update section highlighting
+        if (currentPost && currentPost.sections.length > 0) {
+            var newCurrentSection = null;
+
+            for (var i = 0; i < currentPost.sections.length; i++) {
+                var section = currentPost.sections[i];
+                var sectionTop = section.element.offset().top - 100;
+
+                if (scrollPos >= sectionTop) {
+                    newCurrentSection = section;
+                } else {
+                    break;
+                }
+            }
+
+            // Update section highlighting
+            if (newCurrentSection !== currentSection) {
+                // Remove previous highlight
+                if (currentSection) {
+                    currentSection.link.removeClass('active');
+                }
+
+                // Add new highlight
+                if (newCurrentSection) {
+                    newCurrentSection.link.addClass('active');
+                }
+
+                currentSection = newCurrentSection;
+            }
+        }
+    }
+
+    // Throttle scroll events
+    var scrollTimeout;
+    $(window).on('scroll', function() {
+        if (scrollTimeout) {
+            clearTimeout(scrollTimeout);
+        }
+        scrollTimeout = setTimeout(updateHomepageScrollspy, 50);
+    });
+
+    // Initial update
+    updateHomepageScrollspy();
+});
